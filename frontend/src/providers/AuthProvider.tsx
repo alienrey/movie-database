19252a456cef9fe7a4a1fa03bc5f4d1ed0091112
Alanray <ealanray@gmail.com>
2024-12-29 23:08:45 +0800

    feat: update state contents

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,9 +1,26 @@
-'use client';
-import React, { createContext, useState, useContext, ReactNode } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import { Authentication } from "@/utils/Authentication";
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
+import { useRouter } from "next/navigation";
+import client from "@/utils/FeathersClient";
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+}
 
 interface AuthContextType {
+  user: User | null;
   isAuthenticated: boolean;
-  login: () => void;
+  login: (email: string, password: string) => void;
   logout: () => void;
 }
 
@@ -12,18 +29,52 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
 
-  const login = () => {
-    setIsAuthenticated(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = async (email: string, password: string) => {
+    const result = await Authentication.login(email, password);
+    setUser(result.user);
   };
 
-  const logout = () => {
-    setIsAuthenticated(false);
+  const logout = async () => {
+    await Authentication.logout();
+    setUser(null);
+    router.push("/");
   };
+
+  const getAuthStatus = () => {
+    return Authentication.isAuthenticated();
+  };
+
+  useEffect(() => {
+    const reauthenticate = async () => {
+      const token = window.localStorage.getItem("feathers-jwt");
+      if (!client.authentication.authenticated) {
+        if (!token) {
+          router.push("/auth/signin");
+        } else {
+          try {
+            await client.authentication.setAccessToken(token);
+            const { user } = await client.reAuthenticate();
+            setUser(user);
+          } catch (error) {
+            console.error("Reauthentication failed:", error);
+            router.push("/auth/signin");
+          }
+        }
+      }
+    };
+    reauthenticate();
+  }, [router, Authentication.isAuthenticated()]);
+
+  console.log(Authentication.isAuthenticated());
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider
+      value={{ user, isAuthenticated: getAuthStatus(), login, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
