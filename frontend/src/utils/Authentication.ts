246f8ed3425205c739d7d8b7c875dacb07812ccc
Alanray <ealanray@gmail.com>
2024-12-29 23:08:18 +0800

    feat: add feathers client and authentication util

---
+++ frontend/src/utils/Authentication.ts
@@ -0,0 +1,29 @@
+import client from "./FeathersClient";
+
+export class Authentication {
+  static async login(email: string, password: string) {
+
+    console.log(email, password)
+    const response = await client.authenticate({
+      strategy: "local",
+      email,
+      password,
+    });
+
+    console.log(response)
+
+    return response;
+  }
+
+  static async logout() {
+    return client.logout();
+  }
+
+  static async reauthenticate() {
+    return client.reAuthenticate()
+  }
+
+  static isAuthenticated() {
+    return client.authentication.authenticated
+  }
+}
--- frontend/src/utils/FeathersClient.ts
+++ frontend/src/utils/FeathersClient.ts
@@ -5,22 +5,20 @@ import authentication from "@feathersjs/authentication-client";
 
 const client = feathers();
 
-const uri =
-  process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
+const uri = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
 
 const socket = io(uri, {
   transports: ["websocket"],
+  forceNew: true,
 });
 
 client.configure(socketio(socket));
 
-if (typeof window !== "undefined") {
-  client.configure(
-    authentication({
-      storage: window.localStorage,
-      jwtStrategy: "jwt",
-    })
-  );
-}
+client.configure(
+  authentication({
+    storage: window.localStorage,
+    jwtStrategy: "jwt",
+  })
+);
 
 export default client;
