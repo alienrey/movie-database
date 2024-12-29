9c3fb15d657ff7d082794cb234c29f24e4bf4c71
Alanray <ealanray@gmail.com>
2024-12-29 23:06:43 +0800

    feat: create movies page

--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -1,8 +1,85 @@
-export default function Movies() {
-    return (
-      <div>
-        <h1> Movies </h1>
-      </div>
-    );
-  }
-  
+"use client";
+import { useAuth } from "@/providers/AuthProvider";
+import { Box, Button, Typography } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { colors } from "@/providers/ThemeProvider";
+import { useRouter } from "next/navigation";
+
+export default function MoviesPage() {
+  const { logout, isAuthenticated } = useAuth();
+  const router = useRouter();
+  const handleLogout = () => {
+    console.log("Logout clicked");
+    logout();
+  };
+
+  console.log(isAuthenticated);
+
+  const handleAddMovie = () => {
+    router.push("/movies/create");
+    console.log("Add a new movie clicked");
+  };
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#023047",
+        color: "#fff",
+        position: "relative",
+        padding: 3,
+      }}
+    >
+      <Button
+        onClick={handleLogout}
+        endIcon={<LogoutIcon />}
+        sx={{
+          position: "absolute",
+          top: 16,
+          right: 16,
+          color: colors.white,
+          fontWeight: "bold",
+          fontSize: "0.7rem",
+          "&:hover": {
+            color: colors.fontColor,
+          },
+        }}
+      >
+        Logout
+      </Button>
+
+      <Typography variant="h4" sx={{ mb: 2, fontWeight: "bold" }}>
+        Your movie list is empty
+      </Typography>
+      <Button
+        onClick={handleAddMovie}
+        variant="contained"
+        sx={{
+          borderRadius: "0.5rem",
+          backgroundColor: colors.success,
+          color: colors.white,
+          fontWeight: "bold",
+          "&:hover": {
+        backgroundColor: colors.highlightedSuccess,
+          },
+        }}
+      >
+        Add a new movie
+      </Button>
+
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: "100px",
+          background: "linear-gradient(180deg, transparent, rgba(0,0,0,0.5))",
+        }}
+      ></Box>
+    </Box>
+  );
+}
