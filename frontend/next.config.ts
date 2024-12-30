6d20600b3ef6a83cc67d0aa7df914d50dd182d57
Alanray <ealanray@gmail.com>
2024-12-30 20:32:44 +0800

    feat: add aws s3 endpoint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['movie-db-storage.s3.ap-southeast-1.amazonaws.com'],
+  }
   /* config options here */
 };
 
