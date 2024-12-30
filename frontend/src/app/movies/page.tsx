35221f9a9c19417c54ab225def5eb64af79aa8a5
Alanray <ealanray@gmail.com>
2024-12-30 20:33:30 +0800

    feat: add inital movies list

--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -6,10 +6,11 @@ import { colors } from "@/providers/ThemeProvider";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useMovies } from "@/providers/MoviesProvider";
+import Image from "next/image";
 
 export default function MoviesPage() {
   const { logout } = useAuth();
-  const { fetchMovies } = useMovies();
+  const { fetchMovies, movies } = useMovies();
   const router = useRouter();
 
   useEffect(() => {
@@ -24,6 +25,8 @@ export default function MoviesPage() {
     router.push("/movies/create");
   };
 
+  console.log(movies);
+
   return (
     <Box
       sx={{
@@ -32,12 +35,34 @@ export default function MoviesPage() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#023047",
         color: "#fff",
         position: "relative",
         padding: 3,
       }}
     >
+      {movies.map((movie) => (
+        <Box
+          key={movie.id}
+          sx={{ display: "flex", alignItems: "center", mb: 2 }}
+        >
+          <Box sx={{ width: "100px", height: "150px", mr: 2 }}>
+            <Image
+              src={movie.poster}
+              alt={movie.title}
+              width="100"
+              height="100"
+              style={{ objectFit: "cover" }}
+            />
+          </Box>
+          <Box>
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              {movie.title}
+            </Typography>
+            <Typography variant="body1">{movie.year}</Typography>
+          </Box>
+        </Box>
+      ))}
+
       <Button
         onClick={logout}
         endIcon={<LogoutIcon />}
@@ -68,22 +93,12 @@ export default function MoviesPage() {
           color: colors.white,
           fontWeight: "bold",
           "&:hover": {
-        backgroundColor: colors.highlightedSuccess,
+            backgroundColor: colors.highlightedSuccess,
           },
         }}
       >
         Add a new movie
       </Button>
-
-      <Box
-        sx={{
-          position: "absolute",
-          bottom: 0,
-          width: "100%",
-          height: "100px",
-          background: "linear-gradient(180deg, transparent, rgba(0,0,0,0.5))",
-        }}
-      ></Box>
     </Box>
   );
 }
