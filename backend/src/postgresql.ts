8a34a8a4d4d261ac1a175bb7c8ce5759af838e86
Alanray <ealanray@gmail.com>
2024-12-31 17:38:18 +0800

    feat: add support for ssl

--- backend/src/postgresql.ts
+++ backend/src/postgresql.ts
@@ -2,6 +2,7 @@
 import knex from 'knex'
 import type { Knex } from 'knex'
 import type { Application } from './declarations'
+import fs from 'fs'
 
 declare module './declarations' {
   interface Configuration {
@@ -10,7 +11,21 @@ declare module './declarations' {
 }
 
 export const postgresql = (app: Application) => {
-  const config = app.get('postgresql')
+  let config: any = app.get('postgresql')
+  const ca = fs.readFileSync('./config/ap-southeast-1-bundle.pem').toString()
+  config = {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DB_CONNECTION_STRING,
+      ssl: {
+        ca: fs.readFileSync('./config/ap-southeast-1-bundle.pem').toString()
+      }
+    },
+    pool: {
+      min: 14,
+      max: 22,
+    },
+  };
   const db = knex(config!)
 
   app.set('postgresqlClient', db)
